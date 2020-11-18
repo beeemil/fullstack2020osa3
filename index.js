79a2333e108c17e56ec0e6d3c79c796fc2c2199f
beeemil <eemil.becker@helsinki.fi>
2020-11-18 12:17:45 +0200

    changed index.js app.listen

--- index.js
+++ index.js
@@ -92,5 +92,6 @@ app.get('/api/persons', (req, res) => {
 })
 
 const PORT = process.env.PORT || 3001 
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

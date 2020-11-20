c2481344a2d3c8b4893430249fb21619b137a6e4
beeemil <eemil.becker@helsinki.fi>
2020-11-20 22:06:11 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 require('dotenv').config()
 
-
-const http = require('http')
 const express = require('express')
 const app = express()
-const { brotliCompress } = require('zlib')
-const { response } = require('express')
+const bodyParser = require('body-parser')
 
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
+
 
 
 app.use(express.json())
@@ -17,72 +15,82 @@ app.use(express.urlencoded())
 app.use(morgan('dev'))
 app.use(cors())
 app.use(express.static('build'))
-
-
-app.post('/api/persons',(req,res) => {
-    console.log('body',req.body)
-    const body = req.body
-    const names = persons.map(person => person.name)
-    if (!body.name) {
-        return res.status(400).json({
-            error: 'name missing'
-        })
-    } else if (!body.number) {
-        return res.status(400).json({
-            error: 'number missing'
-        })
-    
-    } 
-    // else if (names.includes(body.name)){
-    //     return res.status(400).json({
-    //         error: 'Name already in the phonebook'
-    //     })
-    // }
-
-    const person = new Person ({
-        name: body.name,
-        number: body.number,
-        id: Math.floor(Math.random() * 100000)
-    })
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
-})
-
-// app.delete('/api/persons/:id', (req, res) => {
-//     const id = Number(req.params.id)
-//     const filPersons = persons.filter(pers => pers.id !== id)
-//     res.status(204).end()
-// })
-
-app.get('/api/persons/:id', (req, res) => {
-    Person.findById(req.params.id).then(person => {
-        res.json(person)
-    })
-    // const id = Number(req.params.id)
-    // console.log('id',id)
-    // const person = persons.find(per => per.id === id)
-    // if (person) {
-    //     res.json(person)
-    // } else {
-    //     res.status(404).end()
-    // }
-})
-
-
-// app.get('/info', (req, res) => {
-//     const nPersons = persons.length
-//     const date = new Date()
-//     res.send(`<p>Phonebook has info for ${nPersons} peple</p><p>${date}</p>`)
-// })
+app.use(bodyParser.json())
 
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then(persons => {
-        res.json(persons.toJSON())
-    })
+	Person.find({}).then(persons => {
+		res.json(persons)
+	})
 })
 
-const PORT = process.env.PORT
+app.post('/api/persons',(req, res, next) => {
+	console.log('body',req.body)
+	const body = req.body
+	const person = new Person ({
+		name: body.name,
+		number: body.number,
+	})
+	person.save().then(savedPerson => {
+		res.json(savedPerson)
+	})
+		.catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+	const body = req.body
+	console.log('Body',body)
+	const person = {
+		name: body.name,
+		number: body.number,
+	}
+
+	Person.findByIdAndUpdate(req.params.id, person, {new: true})
+		.then(updatedPerson => {
+			res.json(updatedPerson)
+		})
+		.catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+	Person.findByIdAndRemove(req.params.id)
+		// eslint-disable-next-line no-unused-vars
+		.then(_result => {
+			res.status(204).end()
+		})
+		.catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+	Person.findById(req.params.id)
+		.then(person => {
+			if (person) {
+				res.json(person)
+			} else {
+				res.status(404).end()
+			}
+		})
+		.catch(error => next(error))
+})
+
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+	console.error(error.message)
+	if (error.name === 'CastError' && error.kind === 'ObjectId'){
+		return res.status(400).send({error: 'malformatted id'})
+	} else if (error.name === 'ValidationError') {
+		return res.status(400).json({error: error.message})
+	}
+	next(error)
+}
+app.use(errorHandler)
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const uniqueValidator = require('mongoose-unique-validator')
 const url = process.env.MONGODB_URI
 
 console.log('Connecting to ', url)
@@ -13,9 +13,11 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
     })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {type: String, required: true, unique: true},
+  number: {type: String, required: true},
 })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
